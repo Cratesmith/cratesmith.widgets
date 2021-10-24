2559258711745aa10bf9bf7d92edf3f4bdd6a9ac
Kiera Lord <cratesmith@cratesmith.com>
2021-10-24 21:35:55 +1000

    added grid layout

---
+++ Widgets/WGridLayout.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.cratesmith.widgets
+{
+	public struct SGridLayout : IWidgetStateLayoutGroup<WidgetGridLayout>, IEquatable<SGridLayout>
+    {
+        public Optional<bool> autoDisableLayoutGroup { get; set; }
+        public WidgetRectTransform rectTransform { get; set; }
+        public WidgetLayoutElement layoutElement { get; set; }
+        public WidgetContentSizeFitter contentSizeFitter { get; set; }
+        public Optional<LogLevel> debugLogging { get; set; }
+        public WidgetGridLayout layoutGroup { get; set; }
+
+        public bool Equals(SGridLayout other)
+        {
+	        return autoDisableLayoutGroup.Equals(other.autoDisableLayoutGroup) && rectTransform.Equals(other.rectTransform) && layoutElement.Equals(other.layoutElement) && contentSizeFitter.Equals(other.contentSizeFitter) && debugLogging.Equals(other.debugLogging) && layoutGroup.Equals(other.layoutGroup);
+        }
+        public override bool Equals(object obj)
+        {
+	        return obj is SGridLayout other && Equals(other);
+        }
+        public override int GetHashCode()
+        {
+	        unchecked
+	        {
+		        var hashCode = autoDisableLayoutGroup.GetHashCode();
+		        hashCode = (hashCode * 397) ^ rectTransform.GetHashCode();
+		        hashCode = (hashCode * 397) ^ layoutElement.GetHashCode();
+		        hashCode = (hashCode * 397) ^ contentSizeFitter.GetHashCode();
+		        hashCode = (hashCode * 397) ^ debugLogging.GetHashCode();
+		        hashCode = (hashCode * 397) ^ layoutGroup.GetHashCode();
+		        return hashCode;
+	        }
+        }
+        public static bool operator ==(SGridLayout left, SGridLayout right)
+        {
+	        return left.Equals(right);
+        }
+        public static bool operator !=(SGridLayout left, SGridLayout right)
+        {
+	        return !left.Equals(right);
+        }
+    }
+	
+	public class WGridLayout : WGridLayout<SGridLayout>
+	{
+	}
+	
+	public class WGridLayout<TState>
+		: WidgetBehaviour<TState>
+		where TState : struct, IWidgetStateLayoutGroup<WidgetGridLayout> //, IEquatable<TState>
+	{
+		[SerializeField]                          bool            m_AutoDisableLayoutGroup = true;
+		[ReadOnlyField, SerializeField] protected GridLayoutGroup m_GridLayoutGroup;
+
+		protected override void Awake()
+		{
+			this.EnsureComponent(ref m_GridLayoutGroup);
+
+			base.Awake();
+		}
+
+		protected override void OnRefresh(ref WidgetBuilder builder)
+		{
+			base.OnRefresh(ref builder);
+
+			var prefab = this.GetPrefab();
+			if (State.layoutGroup.Apply(m_GridLayoutGroup, this.GetPrefab().m_GridLayoutGroup, UsesTypePrefab) || !HasRefreshed)
+				LayoutRebuilder.MarkLayoutForRebuild(RectTransform); // do this beforehand so LayoutGroup.DelayedSetDirty isn't called.
+
+			if (State.autoDisableLayoutGroup.GetValue(prefab.m_AutoDisableLayoutGroup, UsesTypePrefab))
+				m_GridLayoutGroup.enabled = CalcNeedsLayoutGroup();
+		}
+
+		bool CalcNeedsLayoutGroup()
+		{
+			bool needsLayoutGroup = !m_LayoutElement.ignoreLayout;
+			if (!needsLayoutGroup)
+			{
+				foreach (var child in Children)
+				{
+					if (!child.LayoutElement.ignoreLayout)
+					{
+						needsLayoutGroup = true;
+						break;
+					}
+				}
+			}
+			return needsLayoutGroup;
+		}
+	}
+}
---
+++ Widgets/WidgetGridLayout.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.cratesmith.widgets
+{
+	public struct WidgetGridLayout : IEquatable<WidgetGridLayout>
+	{
+		public Optional<GridLayoutGroup.Corner>     startCorner;
+		public Optional<GridLayoutGroup.Axis>       startAxis;
+		public Optional<Vector2>                    cellSize;
+		public Optional<Vector2>                    spacing;
+		public Optional<GridLayoutGroup.Constraint> constraint;
+		public Optional<int>                        constraintCount;
+		public Optional<WidgetRectOffset>           padding;
+		public Optional<TextAnchor>                 childAlignment;
+
+		public WidgetGridLayout AsDefault()
+		{
+			return new WidgetGridLayout()
+			{
+				startCorner = this.startCorner.AsDefault(),
+				startAxis = this.startAxis.AsDefault(),
+				cellSize = this.cellSize.AsDefault(),
+				spacing = this.spacing.AsDefault(),
+				constraint = this.constraint.AsDefault(),
+				constraintCount = this.constraintCount.AsDefault(),
+				padding = this.padding.AsDefault(),
+				childAlignment = this.childAlignment.AsDefault(),
+			};
+		}
+        
+		public bool Apply(GridLayoutGroup to, GridLayoutGroup defaults, bool usesTypePrefab)
+		{
+			bool changed = false;
+			var _padding = padding.GetValue(defaults.padding, usesTypePrefab);
+			var _startCorner = startCorner.GetValue(defaults.startCorner, usesTypePrefab);
+			var _startAxis = startAxis.GetValue(defaults.startAxis, usesTypePrefab);
+			var _cellSize = cellSize.GetValue(defaults.cellSize, usesTypePrefab);
+			var _spacing = spacing.GetValue(defaults.spacing, usesTypePrefab);
+			var _constraint = constraint.GetValue(defaults.constraint, usesTypePrefab);
+			var _constraintCount = constraintCount.GetValue(defaults.constraintCount, usesTypePrefab);
+			var _childAlignment = childAlignment.GetValue(defaults.childAlignment, usesTypePrefab);
+			
+			if(_padding != to.padding) { _padding.ApplyTo(to.padding); changed=true;}
+			if(_startCorner != to.startCorner) { to.startCorner = _startCorner; changed=true;}
+			if(_startAxis != to.startAxis) { to.startAxis = _startAxis; changed=true;}
+			if(_cellSize != to.cellSize) { to.cellSize = _cellSize; changed=true;}
+			if(_spacing != to.spacing) { to.spacing = _spacing; changed=true;}
+			if(_constraint != to.constraint) { to.constraint = _constraint; changed=true;}
+			if(_constraintCount != to.constraintCount) { to.constraintCount = _constraintCount; changed=true;}
+			if(_childAlignment != to.childAlignment) { to.childAlignment = _childAlignment; changed=true;}
+			return changed;
+		}
+		
+		public override bool Equals(object obj)
+		{
+			return obj is WidgetGridLayout other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = startCorner.GetHashCode();
+				hashCode = (hashCode * 397) ^ startAxis.GetHashCode();
+				hashCode = (hashCode * 397) ^ cellSize.GetHashCode();
+				hashCode = (hashCode * 397) ^ spacing.GetHashCode();
+				hashCode = (hashCode * 397) ^ constraint.GetHashCode();
+				hashCode = (hashCode * 397) ^ constraintCount.GetHashCode();
+				hashCode = (hashCode * 397) ^ padding.GetHashCode();
+				hashCode = (hashCode * 397) ^ childAlignment.GetHashCode();
+				return hashCode;
+			}
+		}
+		
+		public bool Equals(WidgetGridLayout other)
+		{
+			return startCorner.Equals(other.startCorner) 
+			       && startAxis.Equals(other.startAxis) 
+			       && cellSize.Equals(other.cellSize) 
+			       && spacing.Equals(other.spacing) 
+			       && constraint.Equals(other.constraint) 
+			       && constraintCount.Equals(other.constraintCount) 
+			       && padding.Equals(other.padding) 
+			       && childAlignment.Equals(other.childAlignment);
+		}
+	}
+}
