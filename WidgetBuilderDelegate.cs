f8a1de86e7e88f6c80eeddd95c6bca62ca2b40c8
Kiera Lord <cratesmith@cratesmith.com>
2022-07-13 15:52:54 +1000

    moving files that are in the wrong place

--- WidgetBuilderDelegate.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace com.cratesmith.widgets
-{
-    /// <summary>
-    /// Delegate used by WidgetBuilder when refreshing widgets 
-    /// </summary>
-    // public delegate void WidgetBuilderDelegate(ref WidgetBuilder builder);
-}
